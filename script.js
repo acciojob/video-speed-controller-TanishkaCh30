2cc0fab039a4cfcdf980ee4b464d087491f0ca3a
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-03-20 17:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+let fwdBtn = document.getElementById("fwd");
+  let  revBtn = document.getElementById("rev");
+let video = document.querySelector(".flex");
+
+fwdBtn.addEventListener("click",()=>{
+	
+	video.currentTime += 25;
+})
+
+
+
+revBtn.addEventListener("click",()=>{
+	video.currentTime-=10;
+})
